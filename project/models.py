4d72bf786c46bcac06725f2466b684f27447f655
lokeshdangii <lokeshdangi1045@gmail.com>
2023-11-03 17:06:21 +0530

    the user model

---
+++ project/models.py
@@ -0,0 +1,10 @@
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+# The UserMixin will add Flask-Login attributes to the model so that Flask-Login will be able to work with it.
